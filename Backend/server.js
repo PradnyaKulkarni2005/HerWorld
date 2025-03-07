595e91f75c1103071cd62cbff685f27b606e0acd
Pradnya Kulkarni <157954530+PradnyaKulkarni2005@users.noreply.github.com>
2025-03-07 23:39:44 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const app = express();
 app.use(cors(
   {
-    origin:[""],
+    origin:["https://her-world-women.vercel.app/"],
     methods:["POST","GET"],
     credentials:true
   }
